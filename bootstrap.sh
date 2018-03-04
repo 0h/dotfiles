6b4f3d04d7d8f0e4c7e1e0a4688f393e2c1bd6a9
halil bayrakci <halilb@gmail.com>
2018-03-04 06:21:26 +0300

    fix

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,6 +11,13 @@ echo "...done"
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
+#install oh-my-zsh
+echo "installing oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp lambda-mod.zsh-theme ~/.oh-my-zsh/themes/
+echo "...done"
+#install fixedsys excelsior font
+cp FSEX302.ttf /Library/Fonts
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
   echo "Moving any existing dotfiles from ~ to $olddir"
